8523d89ecb51b540f13d1c24688ea7f731112a57
Tadas G <gedminast@gmail.com>
2024-03-25 08:52:07 +0200

    add initial controller methods

--- src/Squares.Api/Controllers/GetPointsController.cs
+++ src/Squares.Api/Controllers/GetPointsController.cs
@@ -1,29 +1,101 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Drawing;
+using Squares.Api.Models;
 
 namespace Squares.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class GetPointsController : ControllerBase
+    public class PointsController : ControllerBase
     {   
+        private readonly ILogger<PointsController> _logger;
 
-        private readonly ILogger<GetPointsController> _logger;
-
-        public GetPointsController(ILogger<GetPointsController> logger)
+        public PointsController(ILogger<PointsController> logger)
         {
             _logger = logger;
         }
-
-        [HttpGet(Name = "Points")]
-        public IEnumerable<Point> Get()
+        /// <summary>
+        /// Gets a list of available collections
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name = "points")]
+        public IEnumerable<Points> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new Point
+            // TODO: Implement
+            return Enumerable.Range(1, 5).Select(index => new Points
             {
-                X = Random.Shared.Next(-20, 55),
-                Y = Random.Shared.Next(-20,55)
+                CollectionId = Guid.NewGuid(),
+                PointsCollection = new List<Point>
+                {
+                    new Point{
+                    X = Random.Shared.Next(-20, 55),
+                    Y = Random.Shared.Next(-20, 55)
+                    }
+                }
+               
             })
             .ToArray();
+        }
+
+        /// <summary>
+        /// FR - Import List of points.
+        /// </summary>
+        /// <param name="points"></param>
+        /// <returns></returns>
+        [HttpPut(Name = "points")]
+        public Points Put(Points points)
+        {
+            // TODO: Implement
+            points.CollectionId =  Guid.NewGuid();
+            return points;
+        }
+
+        /// <summary>
+        /// FR - add point to existing list.
+        /// </summary>
+        /// <param name="points"></param>
+        /// <returns></returns>
+        [HttpPut(Name = "points/{collectionId}")]
+        public Points AddPoint(Guid collectionId, Point newPoint)
+        {
+            // TODO: Implement
+            var points = new Points
+            {
+                CollectionId = collectionId,
+                PointsCollection = new List<Point> { newPoint }
+            };
+            return points;
+        }
+
+        /// <summary>
+        /// FR - delete point from existing list.
+        /// </summary>
+        /// <param name="points"></param>
+        /// <returns></returns>
+        [HttpDelete(Name = "points/{collectionId}/deletepoint")]
+        public Points DeletePoint(Guid collectionId, Point newPoint)
+        {
+            // TODO: Implement
+            var points = new Points
+            {
+                CollectionId = collectionId
+            };
+            return points;
+        }
+
+        /// <summary>
+        /// FR - retrieve squares.
+        /// </summary>
+        /// <param name="points"></param>
+        /// <returns></returns>
+        [HttpGet(Name = "Points/{collectionId}/squares")]
+        public Points GetSquares(Guid collectionId, Point newPoint)
+        {
+            // TODO: Implement
+            var points = new Points
+            {
+                CollectionId = collectionId
+            };
+            return points;
         }
     }
 }
---
+++ src/Squares.Api/Models/Points.cs
@@ -0,0 +1,8 @@
+﻿namespace Squares.Api.Models
+{
+    public class Points
+    {
+        public Guid CollectionId { get; set; }
+        public List<Point> PointsCollection { get; set; }
+    }
+}
